29ea36277848c4e107894ae591dca297cb2f0e32
DubeyArpita <arpitadubey23216@gmail.com>
2026-02-21 21:56:43 +0530

    ui update

--- pages/Add_user_page.py
+++ pages/Add_user_page.py
@@ -10,13 +10,14 @@ st.set_page_config(page_title="AltScore | Register", layout="wide", page_icon="�
 
 st.markdown("""
     <style>
-    @import url('https://fonts.googleapis.com/css2?family=Fraunces:ital,wght@0,600;0,700;0,900;1,700&family=Manrope:wght@400;500;600;700;800&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Fraunces:ital,wght@0,600;0,700;0,900;1,700&https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap');
+    
 
     /* ══════════════════════════════════════════
        ROOT — purple/white theme
     ══════════════════════════════════════════ */
     .stApp {
-        background: #f3f0ff;
+        background: #f8d4fa;
         font-family: 'Manrope', sans-serif;
         color: #1e0a3c;
     }
