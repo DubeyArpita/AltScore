3d09ad6e7c9a6b3d6e9c0045e8b71c7d65d52c2e
Aakanksha2309 <aakankshaasharma9@gmail.com>
2026-02-22 00:27:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ section[data-testid="stSidebar"] .stButton > button:hover {
 
     .stApp {
         background: linear-gradient(rgba(0,0,0,0.45), rgba(0,0,0,0.45)),
-                    url("https://unsplash.com/photos/scrabbled-letters-spelling-credit-on-a-wooden-surface-XVOdewJdmEs?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink");
+                    url("https://images.unsplash.com/photo-1705313382153-ed294af4b0a7?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
         background-size: cover;
         background-attachment: fixed;
     }
